da7ee7e797df6f4a22c925810b57629b50f57177
Rafael Almeida Silva <rafalmeidas21@gmail.com>
2022-05-18 13:30:22 -0300

    ADD testes com Jest e mock com msw.

--- src/templates/Home/Home.spec.js
+++ src/templates/Home/Home.spec.js
@@ -1,6 +1,117 @@
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+
+import { render, screen, waitForElementToBeRemoved } from '@testing-library/react';
+import { Home } from '.';
+import userEvent from '@testing-library/user-event';
+
+const handlers = [
+  rest.get('*jsonplaceholder.typicode.com*', async (req, res, ctx) => {
+    return res(
+      ctx.json([
+        {
+          userId: 1,
+          id: 1,
+          title: 'title1',
+          body: 'body1',
+          url: 'img1.jpg',
+        },
+        {
+          userId: 2,
+          id: 2,
+          title: 'title2',
+          body: 'body2',
+          url: 'img2.jpg',
+        },
+        {
+          userId: 3,
+          id: 3,
+          title: 'title3',
+          body: 'body3',
+          url: 'img3.jpg',
+        },
+      ]),
+    );
+  }),
+];
+
+const server = setupServer(...handlers);
+
 describe('<Home />', () => {
-  it('is a dummy test', () => {
-    expect(1).toBe(1);
+  beforeAll(() => {
+    server.listen();
+  });
+
+  // antes de executar um teste reseta o server para não acabar afetando outros testes
+  afterEach(() => {
+    server.resetHandlers();
+  });
+
+  afterAll(() => {
+    server.close();
+  });
+
+  it('should render search posts and load more', async () => {
+    render(<Home />);
+
+    expect.assertions(3);
+
+    const noMorePosts = screen.getByText('Não existem posts');
+
+    await waitForElementToBeRemoved(noMorePosts);
+
+    const search = screen.getByPlaceholderText(/type your search/i);
+    expect(search).toBeInTheDocument();
+
+    const images = screen.getAllByRole('img', { name: /title/i });
+    expect(images).toHaveLength(2);
+
+    const button = screen.getByRole('button', { name: /load more posts/i });
+    expect(button).toBeInTheDocument();
+  });
+
+  it('should search for posts', async () => {
+    render(<Home />);
+
+    expect.assertions(11);
+
+    const noMorePosts = screen.getByText('Não existem posts');
+
+    await waitForElementToBeRemoved(noMorePosts);
+
+    const search = screen.getByPlaceholderText(/type your search/i);
+    expect(search).toBeInTheDocument();
+
+    expect(screen.getByRole('heading', { name: 'title1 1' })).toBeInTheDocument();
+    expect(screen.getByRole('heading', { name: 'title2 2' })).toBeInTheDocument();
+    expect(screen.queryByRole('heading', { name: 'title3 3' })).not.toBeInTheDocument();
+
+    userEvent.type(search, 'title1');
+    expect(screen.getByRole('heading', { name: 'title1 1' })).toBeInTheDocument();
+    expect(screen.queryByRole('heading', { name: 'title2 2' })).not.toBeInTheDocument();
+    expect(screen.queryByRole('heading', { name: 'title3 3' })).not.toBeInTheDocument();
+    expect(screen.getByRole('heading', { name: 'Search value: title1' })).toBeInTheDocument();
+
+    userEvent.clear(search);
+    expect(screen.getByRole('heading', { name: 'title1 1' })).toBeInTheDocument();
+    expect(screen.getByRole('heading', { name: 'title2 2' })).toBeInTheDocument();
+
+    userEvent.type(search, 'post do not exists');
+    expect(screen.getByText('Não existem posts')).toBeInTheDocument();
+  });
+
+  it('should load more posts', async () => {
+    render(<Home />);
+
+    expect.assertions(2);
+
+    const noMorePosts = screen.getByText('Não existem posts');
+
+    await waitForElementToBeRemoved(noMorePosts);
+
+    const button = screen.getByRole('button', { name: /load more posts/i });
+    userEvent.click(button);
+    expect(screen.getByRole('heading', { name: 'title3 3' })).toBeInTheDocument();
+    expect(button).toBeDisabled();
   });
 });
-
